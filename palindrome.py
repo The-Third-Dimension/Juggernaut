f0e832e21612c83663c1fe04758328dbd5015608
Gayathri Ramesh <76430931+rameshgayathri@users.noreply.github.com>
2022-07-16 15:23:00 +0530

    Update palindrome.py
    Co-authored-by: Ashwin Raj <rajashwin733@gmail.com>

--- palindrome.py
+++ palindrome.py
@@ -2,8 +2,7 @@
 Leetcode:
 https://leetcode.com/problems/palindrome-number/
 Question:
-Given an integer x, return true if x is palindrome integer.
-An integer is a palindrome when it reads the same backward as forward.
+Given an integer x, return true if x is palindrome integer. An integer is a palindrome when it reads the same backward as forward.
 For example, 121 is a palindrome while 123 is not.
 Example 1:
 Input: x = 121
